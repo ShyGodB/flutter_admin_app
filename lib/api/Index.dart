f98e3b1f5a7e7f62f0326b08a23c7ca42fe34b32
fq <fq@edtuina.com>
2020-08-06 18:02:44 +0800

    1

--- lib/api/Index.dart
+++ lib/api/Index.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 String apiUrl = 'http://10.0.2.2:3101/api/admin'; // android
-// String apiUrl = 'http://127.0.0.1:3101/api/admin'; // ios 
+// String apiUrl = 'http://127.0.0.1:3101/api/admin'; // ios
 // authtoken = "d470c3c5eab81afa2293d504782e5c4d"
 
 dynamic get(String url, data) async {
@@ -20,6 +20,7 @@ dynamic post(String url, data) async {
     'Content-Type': 'application/json',
     'authtoken': authtoken
   };
-  Response res = await Dio().post(reqUrl, data: data, options: Options(headers: httpHeaders));
+  Response res = await Dio()
+      .post(reqUrl, data: data, options: Options(headers: httpHeaders));
   return res.data;
 }
