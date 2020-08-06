b02590a0573b686851120dc30fdb27d604685505
fq <fq@edtuina.com>
2020-08-06 18:16:45 +0800

    1

--- lib/api/Index.dart
+++ lib/api/Index.dart
@@ -1,26 +1,30 @@
 import 'package:dio/dio.dart';
+// import 'package:flutter_admin_app/config/index.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:flutter_admin_app/config/$global.env';
 
 String apiUrl = 'http://10.0.2.2:3101/api/admin'; // android
+
 // String apiUrl = 'http://127.0.0.1:3101/api/admin'; // ios
 // authtoken = "d470c3c5eab81afa2293d504782e5c4d"
+class API {
+  dynamic get(String url, data) async {
+    String reqUrl = apiUrl + url;
+    Response res = await Dio().get(reqUrl, queryParameters: data);
+    return res.data;
+  }
 
-dynamic get(String url, data) async {
-  String reqUrl = apiUrl + url;
-  Response res = await Dio().get(reqUrl, queryParameters: data);
-  return res.data;
-}
-
-dynamic post(String url, data) async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String authtoken = prefs.getString('authtoken');
-  String reqUrl = apiUrl + url;
-  Map<String, dynamic> httpHeaders = {
-    'Accept': 'application/json,*/*',
-    'Content-Type': 'application/json',
-    'authtoken': authtoken
-  };
-  Response res = await Dio()
-      .post(reqUrl, data: data, options: Options(headers: httpHeaders));
-  return res.data;
+  dynamic post(String url, data) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String authtoken = prefs.getString('authtoken');
+    String reqUrl = apiUrl + url;
+    Map<String, dynamic> httpHeaders = {
+      'Accept': 'application/json,*/*',
+      'Content-Type': 'application/json',
+      'authtoken': authtoken
+    };
+    Response res = await Dio()
+        .post(reqUrl, data: data, options: Options(headers: httpHeaders));
+    return res.data;
+  }
 }
